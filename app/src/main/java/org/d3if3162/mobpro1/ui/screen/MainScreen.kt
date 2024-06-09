1bbd67edcdb910393cbd539798e3a7b8f3d461b6
Rafa Suryaputra <rafasuryaputra51@gmail.com>
2024-06-10 05:34:45 +0700

    Membuat indikator loading image

--- app/src/main/java/org/d3if3162/mobpro1/ui/screen/MainScreen.kt
+++ app/src/main/java/org/d3if3162/mobpro1/ui/screen/MainScreen.kt
@@ -22,6 +22,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
@@ -87,6 +88,7 @@ fun ListItem(kucing: Kucing) {
                 .build(),
             contentDescription = stringResource(R.string.gambar, kucing.nama),
             contentScale = ContentScale.Crop,
+            placeholder = painterResource(id = R.drawable.loading_img),
             modifier = Modifier
                 .fillMaxWidth()
                 .padding(4.dp)
@@ -115,7 +117,7 @@ fun ListItem(kucing: Kucing) {
 @Preview(showBackground = true)
 @Preview(uiMode = Configuration.UI_MODE_NIGHT_YES, showBackground = true)
 @Composable
-fun MainScreenPreview() {
+fun GreetingPreview() {
     Mobpro1Theme {
         MainScreen()
     }

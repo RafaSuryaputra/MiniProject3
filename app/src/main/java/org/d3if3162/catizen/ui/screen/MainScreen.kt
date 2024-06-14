c5cefbf5b7f7b2b74c7f24c09998fdf074aa393b
Rafa Suryaputra <rafasuryaputra51@gmail.com>
2024-06-14 14:24:02 +0700

    memperbarui ui

--- app/src/main/java/org/d3if3162/catizen/ui/screen/MainScreen.kt
+++ app/src/main/java/org/d3if3162/catizen/ui/screen/MainScreen.kt
@@ -37,7 +37,6 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
